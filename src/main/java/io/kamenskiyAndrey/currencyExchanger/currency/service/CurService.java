13498af71171b49ab9614790603e599f24b901c1
AndreyJavaEd <scream123456@yandex.ru>
2023-12-02 17:33:27 +0300

    В Классе CurService.java мы написали метод unmarshall(), который принимает xml строку из запроса и преобразует ее в объект класса ValCurs. В методе callAllByCurrentDate() Из объекта ValCurs мы достаем список значений валют и преобразуем его в Мапу с ключем - код валюты, значение - числовое представление валюты.
    Для преобразования в Мапу мы используем написанный вспомогательный метод parseWithLocaleStringValue(), который парсит строчное представление валюты в число (преобразует в BigDecimal).

--- src/main/java/io/kamenskiyAndrey/currencyExchanger/currency/service/CurService.java
+++ src/main/java/io/kamenskiyAndrey/currencyExchanger/currency/service/CurService.java
@@ -3,12 +3,21 @@ package io.kamenskiyAndrey.currencyExchanger.currency.service;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import io.kamenskiyAndrey.currencyExchanger.currency.client.CurrencyDateHttpClient;
+import io.kamenskiyAndrey.currencyExchanger.currency.schemas.ValCurs;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
 import org.springframework.stereotype.Service;
 
+import java.io.StringReader;
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 @Service
 public class CurService {
@@ -20,6 +29,7 @@ public class CurService {
         this.client = client;
     }
 
+    //Метод получения списка курса валюты в зависимости от типа валюты (код валюты)
     public BigDecimal requestByCurrencyCode(String currencyCode) {
 
         try {
@@ -28,10 +38,34 @@ public class CurService {
             throw new RuntimeException(e);
         }
     }
-    //TODO
+
+    //Метод получения Мапы с ключем - код валюты типа String и значение - курс валюты типа BigDecimal
     private Map<String, BigDecimal> callAllByCurrentDate() {
         var xml = client.requestByDate(LocalDate.now()); // получим xml - в нем будут котировки всех валют за текщую дату
-        return null;
+        ValCurs valCurs = unmarshall(xml);
+        List<ValCurs.Valute> valute = valCurs.getValute(); //получили список значений валют
+        Map<String, BigDecimal> allValuesOfEachCurrency = valute.stream().collect(Collectors.toMap(ValCurs.Valute::getCharCode
+                , items -> parseWithLocaleStringValue(items.getValue())));
+        return allValuesOfEachCurrency;
     }
 
+    //Метод в котором распарсили String значение курса валюты в BigDecimal
+    private BigDecimal parseWithLocaleStringValue(String currency) {
+        try {
+            double v = NumberFormat.getNumberInstance(Locale.getDefault()).parse(currency).doubleValue(); //преобразуем текстовое представление числа в тип double в соответствии с текущей Локалью
+            return BigDecimal.valueOf(v);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //возвращает объект типа ValCurs, полученный после разбора XML-строки.
+    private ValCurs unmarshall(String xml) {
+        try (StringReader reader = new StringReader(xml)) { //читает данные из строки xml
+            JAXBContext context = JAXBContext.newInstance(ValCurs.class); //создали контекст для класса, соответствующего структуре XML (ValCurs)
+            return (ValCurs) context.createUnmarshaller().unmarshal(reader); //получаем объект типа ValCurs
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
